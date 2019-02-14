58be6f076f1389f8bd3f2ce8556249fd322ad584
Borja <>
2019-02-14 12:37:04 +0100

    Prueba del mapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,35 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+declare var ol: any;
+declare var map: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
+
+  latitude: number = 18.5204;
+  longitude: number = 73.8567;
+
+  map: any;
+
+  ngOnInit() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([73.8567, 18.5204]),
+        zoom: 8
+      })
+    });
+  }
+  /*constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
@@ -22,5 +44,5 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
-  }
+  }*/
 }
