d8e49f5a92fd56b2faf32a7f55db538c948bce84
Jon Bahillo <jonbahillo@gmail.com>
2019-02-18 13:04:49 +0100

    mapa en home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-declare var ol: any;
-declare var map: any;
+/*declare var ol: any;
+declare var map: any;*/
 
 @Component({
   selector: 'app-root',
@@ -12,7 +9,7 @@ declare var map: any;
 })
 export class AppComponent {
 
-  latitude: number = 18.5204;
+ /* latitude: number = 18.5204;
   longitude: number = 73.8567;
 
   map: any;
@@ -29,6 +26,8 @@ export class AppComponent {
         center: ol.proj.fromLonLat([73.8567, 18.5204]),
         zoom: 8
       })
+      
     });
-  }
+  }*/
+  
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+declare var ol: any;
+declare var map: any;
 
 @Component({
   selector: 'app-home',
@@ -7,4 +13,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  map: any;
+
+  ngOnInit() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([73.8567, 18.5204]),
+        zoom: 8
+      })
+      
+    });
+  }
+  
 }
